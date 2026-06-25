1f797c6e98044a5722f6a90826a7ddac7196851d
Anthony140823 <anthonyreyes140803@gmail.com>
2026-06-25 00:40:34 -0500

    mejoras

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ incluyedno un impuesto aplicado.
 
 FORMULA: pago_total = pago_sin_impuesto + pago_sin_impuesto * (impuesto/100)
 '''
+# Función para calcular el pago total
+def calcular_total_pago(pago_sin_impuesto:float, impuesto:float):
+    pago_total = pago_sin_impuesto + pago_sin_impuesto * (impuesto/100)
+    return pago_total
+
+# Ejecutamos la función
 pago_sin_impuesto = float(input("Proporcione el pago sin impuesto: "))
 impuesto = float(input("Proporcione el monto del impuesto: "))
 
-# Función para calcular el pago total
-def total(pago_sin_impuesto:float, impuesto:float):
-    pago_total = pago_sin_impuesto + pago_sin_impuesto * (impuesto/100)
-    print(f'Pago con impuesto: {pago_total:.2f}')
-
-total(pago_sin_impuesto, impuesto)
+pago_con_impuesto = calcular_total_pago(pago_sin_impuesto, impuesto)
+print(f'Pago con impuesto: {pago_con_impuesto:.2f}')
