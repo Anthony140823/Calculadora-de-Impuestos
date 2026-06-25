604dfb0fa0bef4979232f5b3e7554ea7d6e1e11b
Anthony140823 <anthonyreyes140803@gmail.com>
2026-06-24 23:04:12 -0500

    Creación del archivo main

---
+++ main.py
@@ -0,0 +1,8 @@
+'''
+        EJERCICIO: CALCULADORA DE IMPUESTOS
+
+Crear una función para calcular el total de un pago 
+incluyedno un impuesto aplicado.
+
+FORMULA: pago_total = pago_sin_impuesto + pago_sin_impuesto * (impuesto/100)
+'''
