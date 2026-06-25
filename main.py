37aaf9795dc33ffced5c1ef662df2b2d5b9930e3
Anthony140823 <anthonyreyes140803@gmail.com>
2026-06-25 00:24:47 -0500

    código para pedir los datos al usuario

--- main.py
+++ main.py
@@ -6,3 +6,5 @@ incluyedno un impuesto aplicado.
 
 FORMULA: pago_total = pago_sin_impuesto + pago_sin_impuesto * (impuesto/100)
 '''
+pago_sin_impuesto = int(input("Proporcione el pago sin impuesto: "))
+impuesto = int(input("Proporcione el monto del impuesto: "))
